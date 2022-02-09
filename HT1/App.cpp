9a14321ccfadf147bbb23c85f17061ca73be2eda
Gabriel XIcay <gabrielajsivinac@gmail.com>
2022-02-08 21:29:23 -0600

    FILE
    Creando Arhcivo

--- HT1/App.cpp
+++ HT1/App.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <fstream>
+
 using namespace std;
 
 //metodo para el menu principal    
@@ -9,6 +11,34 @@ cout<<"1. Registro Profesor"<<endl;
 cout<<"2. Registro Estudiante"<<endl;
 cout<<"3. Ver Registro"<<endl;
 cout<<"4. Salir"<<endl;
+}
+
+//Struct para los atributos de Profesor
+struct Profesor
+{
+    int Id_profesor;
+    char CUI[13];
+    char Nombre[25];
+    char Curso[25];
+    /* data */
+};
+//Struct para estudiante
+struct Estudiante
+{
+    int Id_estudiante;
+    char CUI[13];
+    char Nombre[25];
+    char Carnet[25];
+    /* data */
+};
+
+//agregar nuevo campo para Profesor
+void agregarProfesor(){
+        ofstream archivo;
+        archivo.open("Profesor.bin",std::ofstream::out | ios::binary);
+        
+        
+
 }
 
 //Inicio del programa
@@ -20,18 +50,39 @@ int main(){
 do{
     
         mainMenu();
+
         cout<<"\nIngrese una opicion: ";
         cin>>select;
+
+        Profesor *profesor = new Profesor;
+        Estudiante *estudiante = new Estudiante;
+
+
         switch (select)
         {
-        case 1 :
-            cout<<"PRESIONO 1"<<endl;        
+                                       
+
+        case 1 ://INGRESAR DATOS STRUCT PROFESOR
+        
+            
+            
+            
             break;
-        case 2:
-            cout<<"PRESIONO 2"<<endl;
+        case 2://INGRESAR DATOS STRUCT ESTUDIANTE
+            cout<<"Ingrese Id del Estudiante : ";
+            cin>>estudiante->Id_estudiante;
+            cout<<"Ingrese CUI del Estudiante : ";
+            cin>>estudiante->CUI;
+            cout<<"Ingrese Nombre del Estudiante : ";
+            cin>>estudiante->Nombre;
+            cout<<"Ingrese Numero de Carnet : ";
+            cin>>estudiante->Carnet;
+            cout<<endl;
+            cout<<"***DATOS REGISTRADOS***"<<endl;
+ 
             break;
         case 3:
-            cout<<"PRESIONO 3"<<endl;
+            
             break;
         case 4:
             bsalir=true;
